a01a64530e33600728c3461545afd69e966db3a3
TalhaMRao <talha2001m@gmail.com>
2022-11-14 11:07:56 -0500

    inFix to postFix working

--- C/VS Code/CIS2520/A3/infixToPostfix1.c
+++ C/VS Code/CIS2520/A3/infixToPostfix1.c
@@ -1,15 +1,131 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+struct node
+{
+	char data[4];
+	struct node *left;
+	struct node *right;
+	struct node *next;
+};
+
+struct node *head = NULL;
 
 char stack[100];
 int top = -1;
 
-void push(char x)
+void cPush(char x);
+char cPop();
+int cPriority(char x);
+
+int main(int argc, char *argv[])
+{
+	char exp[100];
+	char *e, x;
+	e = argv[1];
+
+	// printf("Enter the expression : ");
+	// scanf("%s", exp);
+	// printf("\n");
+	// e = exp;
+
+	// char *str = "";
+	// size_t len = strlen(str);
+	char *str2 = malloc(strlen(argv[1]) + 1);
+	// strcpy(str2, str);
+	int len = 0;
+	while (*e != '\0')
+	{
+		if (isalpha(*e))
+		{
+			printf("%c", *e);
+			str2[len] = *e;
+			e++;
+			len++;
+
+			printf("%c ", *e);
+			str2[len] = *e;
+			len++;
+		}
+		else if (isdigit(*e))
+		{
+			printf("%c", *e);
+			str2[len] = *e;
+			e++;
+			len++;
+
+			printf("%c", *e);
+			str2[len] = *e;
+			e++;
+			len++;
+
+			printf("%c", *e);
+			str2[len] = *e;
+			e++;
+			len++;
+
+			printf("%c ", *e);
+			str2[len] = *e;
+			len++;
+		}
+		else if (*e == '(')
+			cPush(*e);
+		else if (*e == ')')
+		{
+			while ((x = cPop()) != '(')
+			{
+				printf("%c ", x);
+				str2[len] = x;
+				len++;
+			}
+		}
+		else
+		{
+			while (cPriority(stack[top]) >= cPriority(*e))
+			{
+				printf("%c ", x);
+				str2[len] = x;
+				len++;
+			}
+			cPush(*e);
+		}
+		e++;
+	}
+	while (top != -1)
+	{
+		x = cPop();
+		printf("%c ", x);
+		str2[len] = x;
+		len++;
+	}
+
+	str2[len] = '\0';
+
+	printf("\n\n%s\n\n", str2);
+
+	free(str2);
+	return 0;
+}
+
+struct node *newNode(char *data)
+{
+	struct node *node = (struct node *)malloc(sizeof(struct node));
+	strcpy(node->data, data);
+	node->left = NULL;
+	node->right = NULL;
+	node->next = NULL;
+
+	return (node);
+}
+
+void cPush(char x)
 {
 	stack[++top] = x;
 }
 
-char pop()
+char cPop()
 {
 	if (top == -1)
 		return -1;
@@ -17,7 +133,7 @@ char pop()
 		return stack[top--];
 }
 
-int priority(char x)
+int cPriority(char x)
 {
 	if (x == '(')
 		return 0;
@@ -25,41 +141,5 @@ int priority(char x)
 		return 1;
 	if (x == '*' || x == '/')
 		return 2;
-	return 0;
-}
-
-int main()
-{
-	char exp[100];
-	char *e, x;
-	printf("Enter the expression : ");
-	scanf("%s", exp);
-	printf("\n");
-	e = exp;
-
-	while (*e != '\0')
-	{
-		if (isalnum(*e))
-			printf("%c ", *e);
-		else if (*e == '(')
-			push(*e);
-		else if (*e == ')')
-		{
-			while ((x = pop()) != '(')
-				printf("%c ", x);
-		}
-		else
-		{
-			while (priority(stack[top]) >= priority(*e))
-				printf("%c ", pop());
-			push(*e);
-		}
-		e++;
-	}
-
-	while (top != -1)
-	{
-		printf("%c ", pop());
-	}
 	return 0;
 }
