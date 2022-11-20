e4d37c8ef6147c26a7730923d31783177b004a07
TalhaMRao <talha2001m@gmail.com>
2022-11-19 23:52:32 -0500

    Adding heaps

--- C/VS Code/CIS2520/A4/a4.c
+++ C/VS Code/CIS2520/A4/a4.c
@@ -13,15 +13,65 @@
 #include <ctype.h>
 #include <string.h>
 
-typedef struct node
+typedef struct MinHeap
 {
-	int sum_key;
-	int key[3];
-	int content[7];
-} node;
+    int *arr;
+    // Current Size of the Heap
+    int size;
+    // Maximum capacity of the heap
+    int capacity;
+} MinHeap;
+
+MinHeap *init_minheap(int capacity);
+void free_minheap(MinHeap *heap);
+int parent(int i);
+int left_child(int i);
+int right_child(int i);
+int get_min(MinHeap *heap);
 
 int main()
 {
 
-	return 0;
+    return 0;
+}
+
+MinHeap *init_minheap(int capacity)
+{
+    MinHeap *minheap = (MinHeap *)calloc(1, sizeof(MinHeap));
+    minheap->arr = (int *)calloc(capacity, sizeof(int));
+    minheap->capacity = capacity;
+    minheap->size = 0;
+    return minheap;
+}
+
+void free_minheap(MinHeap *heap)
+{
+    if (!heap)
+        return;
+    free(heap->arr);
+    free(heap);
+}
+
+int parent(int i)
+{
+    // Get the index of the parent
+    return (i - 1) / 2;
+}
+
+int left_child(int i)
+{
+    return (2 * i + 1);
+}
+
+int right_child(int i)
+{
+    return (2 * i + 2);
+}
+
+int get_min(MinHeap *heap)
+{
+    // Return the root node element,
+    // since that's the minimum, by the min-heap
+    // property
+    return heap->arr[0];
 }
